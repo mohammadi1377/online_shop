c318ff34c1b9455b81f5833509b1e523ea0cea40
fatemeh mohammadi <100574787+mohammadi1377@users.noreply.github.com>
2023-06-16 21:08:09 +0330

    Update views.py

--- order/views.py
+++ order/views.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-=======
 
 from django.shortcuts import render, get_object_or_404
 from order.models import Orders
@@ -11,4 +6,3 @@ from order.models import Orders
 def order(request, id):
     order = get_object_or_404(Orders, pk=id)
     return render(request, "order.html", {"order": order})
->>>>>>> develop
